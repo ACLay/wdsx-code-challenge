ab51ef546696f468c061ec36b154a63eaa57b832
Alex Lay <AlexChrisLay@gmail.com>
2016-09-26 12:29:00 +0100

    added convenience method to load device file directly

--- src/main/java/aclay/codechallenge/json/Reader.java
+++ src/main/java/aclay/codechallenge/json/Reader.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 
 public class Reader {
@@ -14,4 +15,9 @@ public class Reader {
 		return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
 	}
 	
+	public List<Device> readDevicesFromFile(String fileName) throws IOException, DeviceException{
+		String fileContent = readFile(fileName);
+		Parser parser = new Parser();
+		return parser.parseDeviceArray(parser.readJsonArray(fileContent));
+	}
 }
